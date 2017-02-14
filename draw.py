07bca00b0b04878d26fa799aac823537abfc2d50
Ling <lcheng3@stuy.edu>
2017-02-13 21:25:43 -0500

    finished drawing line in octant 2

--- draw.py
+++ draw.py
@@ -1,5 +1,6 @@
 from display import *
 
+#draws in octant 1
 def draw_line( x0, y0, x1, y1, screen, color ):
     x = x0
     y = y0
@@ -15,3 +16,20 @@ def draw_line( x0, y0, x1, y1, screen, color ):
             d = d + B
         x = x + 1
         d = d + A
+
+#draws in octant 2
+def draw_line_2( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = -1 * (x1 - x0)
+    d = 2 * A + B
+    B = 2 * B
+    A = 2 * A
+    while (y <= y1):
+        plot(screen, color, x, y)
+        if d < 0:
+            x = x + 1
+            d = d + A
+        y = y + 1
+        d = d + B
--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from draw import *
 screen = new_screen()
 color = [0, 255, 0]
 
+draw_line(0, 0, 500, 0, screen, color)
 draw_line(0, 0, 500, 250, screen, color)
 draw_line(0, 0, 500, 500, screen, color)
+
+draw_line_2(0, 0, 250, 500, screen, color)
+draw_line_2(0, 0, 0, 500, screen, color)
 
 display(screen)
 save_extension(screen, 'img.png')
